b32b19b7d1cb051b87296b7ebe904024268478ff
Anandu Jith Panickar <anandupanickar27@gmail.com>
2026-06-28 13:59:26 +0530

    Build Image Search Engine

---
+++ Image Search Engine/script.js
@@ -0,0 +1,41 @@
+const searchForm = document.getElementById("search-form");
+const searchResult = document.getElementById("search-result");
+const searchBox = document.getElementById("search-box");
+const showMorebtn = document.getElementById("show-more-btn");
+
+let keyword="";
+let page=1;
+let accessKey="NBcRFBTPPEJwKUOxSlDNIZ65M8g2-z65dNxALqFI2Tk"
+async function searchImage() {
+    keyword=searchBox.value;
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}&per_page=12`;
+    
+    const response = await fetch(url);
+    const data = await response.json();
+
+    const results = data.results;
+    if(page === 1){
+        searchResult.innerHTML="";
+    }
+    results.map((result) =>{
+        const image = document.createElement("img");
+        image.src = result.urls.small;
+        const imageLink = document.createElement("a");
+        imageLink.href = result.links.html;
+        imageLink.target = "_blank";
+        imageLink.appendChild(image);
+        searchResult.appendChild(imageLink);
+    })
+    showMorebtn.style.display="block";
+}
+
+searchForm.addEventListener("submit", (e) =>{
+    e.preventDefault();
+    page = 1;
+    searchImage();
+})
+
+showMorebtn.addEventListener("click", ()=>{
+    page++;
+    searchImage();
+})
